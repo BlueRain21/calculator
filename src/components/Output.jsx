b75d777fb3d6f2596c05cd348556c642d09b3085
BlueRain21 <neha.trycatch@gmail.com>
2025-04-29 16:41:10 +0530

    Image alt

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -1,30 +1,32 @@
-
-
 import { useContext } from "react";
 
 import { MainContext, dispatchMethods } from "../context/MainContext";
-import logo from "/calculator.webp"
+import logo from "/calculator.webp";
 
+const Output = () => {
+  const [{ equation, result }, dispatch] = useContext(MainContext);
 
-const Output = () =>{
-
-    const [{equation, result}, dispatch] = useContext(MainContext)
-
-
-    return (
-        <div className="text-right relative pt-30">
-            {/* <p className="text-[20px] text-muted-blue-grey-light dark:text-muted-blue-grey-dark mx-10 overflow-scroll">{equation}</p> */}
-            <label className="hidden mx-10" htmlFor="inp1">Enter values here</label>
-            <input type="text" value={equation} className="text-[20px] text-muted-blue-grey-light dark:text-muted-blue-grey-dark mx-10 overflow-scroll text-right focus:border-none focus:outline-none" 
-                onChange={(e) => {
-                    dispatch({type: dispatchMethods.input, payload:e.target.value })
-                }}
-                id="inp1"
-            />
-            <p className="pb-10 mx-10 font-medium text-[48px] overflow-scroll">{result}</p>
-            <img className="w-[50px] absolute top-5 left-5 " src={logo}/>
-        </div>
-    )
-}
+  return (
+    <div className="text-right relative pt-30">
+      {/* <p className="text-[20px] text-muted-blue-grey-light dark:text-muted-blue-grey-dark mx-10 overflow-scroll">{equation}</p> */}
+      <label className="mx-10" htmlFor="inp1">
+        <span className="hidden">Enter values here</span>
+        <input
+          type="text"
+          value={equation}
+          className="text-[20px] text-muted-blue-grey-light dark:text-muted-blue-grey-dark mx-10 overflow-scroll text-right focus:border-none focus:outline-none"
+          onChange={(e) => {
+            dispatch({ type: dispatchMethods.input, payload: e.target.value });
+          }}
+          id="inp1"
+        />
+      </label>
+      <p className="pb-10 mx-10 font-medium text-[48px] overflow-scroll">
+        {result}
+      </p>
+      <img className="w-[50px] absolute top-5 left-5 " src={logo} alt="logo" />
+    </div>
+  );
+};
 
 export default Output;
